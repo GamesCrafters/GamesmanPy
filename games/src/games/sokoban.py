df6880152261f9aeb88d3de72302acd1bc5c08c0
kshitij <kshitij.tomar07@berkeley.edu>
2026-04-22 03:14:48 -0700

    refactor: remove all excess code

--- games/src/games/sokoban.py
+++ games/src/games/sokoban.py
@@ -96,6 +96,7 @@ class Sokoban(Game):
                     "    #       ###  ######"
                     "    #########          "
                 )
+                
             #"              "
             #roughly 40 billion state space
             case "6":
@@ -155,7 +156,6 @@ class Sokoban(Game):
             
         px, py = p_idx % self.column_size, p_idx // self.column_size
         
-        # 1. BFS to find all spaces the player can currently reach
         reachable = set()
         queue = [(px, py)]
         
@@ -171,12 +171,11 @@ class Sokoban(Game):
                         if position[n_idx] in [' ', '.']:
                             queue.append((nx, ny))
         
-        # 2. Check all boxes to see if they can be pushed
         # O(nm) check
         for y in range(self.row_size):
             for x in range(self.column_size):
                 idx = y * self.column_size + x
-                if position[idx] in ['$', '*']: # Found a box
+                if position[idx] in ['$', '*']: 
                     for dx, dy in self.dxdy:
                         player_req_x, player_req_y = x - dx, y - dy
                         push_target_x, push_target_y = x + dx, y + dy
@@ -192,23 +191,19 @@ class Sokoban(Game):
         box_idx, dx, dy = move
         pos_list = list(position)
         
-        # 1. Remove player from old position
         p_idx = self.get_pos_idx(position)
         pos_list[p_idx] = '.' if pos_list[p_idx] == '+' else ' '
         
-        # 2. Calculate new coordinates for the pushed box
         bx = box_idx % self.column_size
         by = box_idx // self.column_size
         nx, ny = bx + dx, by + dy 
         n_idx = ny * self.column_size + nx
         
-        # 3. Place the box in its new location
         if pos_list[n_idx] == '.':
             pos_list[n_idx] = '*'
         else:
             pos_list[n_idx] = '$'
             
-        # 4. Move the player into the tile the box just vacated
         if position[box_idx] == '*': 
             pos_list[box_idx] = '+'
         else:
@@ -222,7 +217,6 @@ class Sokoban(Game):
         Returns Value.Lose if any box is deadlocked in a corner.
         Otherwise returns None.
         """
-        # 1. Win Condition
         if position.find('$') == -1:
             return Value.Win
         
@@ -232,7 +226,6 @@ class Sokoban(Game):
                 all_boxes.add(i)
 
         
-        # 2. Corner Deadlock Detection
         for y in range(self.row_size):
             for x in range(self.column_size):
                 idx = y * self.column_size + x
@@ -245,39 +238,6 @@ class Sokoban(Game):
                     if (up or down) and (left or right):
                         return Value.Loss
 
-
-        # while loop should only run a maximum of B times
-        # where B is the number of boxes in the position
-        # can not call generate moves --> messes with autogui
-        # also, just a really slow bfs call in general
-        """
-        while True:
-            moves = self.generate_moves(current_position)
-            new_live_boxes = set(move[0] for move in moves)
-
-            if not new_live_boxes:
-                break
-                
-            live_boxes.update(new_live_boxes)
-
-            pos_list = list(position)
-            for idx in live_boxes:
-                if pos_list[idx] == '$':
-                    pos_list[idx] = ' '
-                elif pos_list[idx] == '*':
-                    pos_list[idx] = '.'
-
-            current_position = "".join(pos_list)
-            
-        
-        dead_boxes = all_boxes - live_boxes
-
-        for idx in dead_boxes:
-            # If a Dead Box is NOT on a goal square ('*'), the state is lost
-            if position[idx] != '*':
-                return Value.Loss
-        """
-
         return None
 
 
@@ -289,7 +249,6 @@ class Sokoban(Game):
 
         p_idx = self.get_pos_idx(position)
             
-        #optimize this further by maybe storing this?
         box_indices = [i for i, char in enumerate(position) if char in ['$', '*']]
         
         box_indices.sort(reverse=True)
@@ -301,51 +260,11 @@ class Sokoban(Game):
             k = total_boxes - i  
             box_rank += math.comb(c, k)
             
-        # prevent overlap from player index by getting max int
         max_p_idx = self.column_size * self.row_size 
         
         hash = (box_rank * max_p_idx) + p_idx
         
         return hash
-    
-        """
-    #optimize hash because this is not going to store level 6 or beyond
-    #it takes more than 64 bits so that is incredibly chopped
-    #lowk the hash is supposed to hash every state, so if a position has 20,000,000 states
-    #you should be able to just hash that since 2^64 = 10^19
-    def hash_ext(self, position: str) -> int:
-        box_rank = get_combinadic_rank(box_indices)
-        perfect_hash = (box_rank * total_valid_tiles) + valid_p_idx
-        """
-      
-        """
-        Returns a perfect hash using bitpacking.
-        Only dynamic elements (Player and Boxes) are stored.
-
-        # 1. Find the player's index
-        p_idx = self.get_pos_idx(position)
-            
-        # 2. Find all box indices ('$' for floor, '*' for goal)
-        box_indices = []
-        for i, char in enumerate(position):
-            if char in ['$', '*']:
-                box_indices.append(i)
-                
-        # 3. Sort the boxes
-        box_indices.sort()
-        
-        # 4. Calculate how many bits we need per index
-        # For an 8x8 board (64 tiles, max index 63), this returns 6 bits.
-        max_index = self.column_size * self.row_size - 1
-        bits_per_index = max_index.bit_length() 
-        
-        packed_hash = p_idx
-        
-        for b_idx in box_indices:
-            packed_hash = (packed_hash << bits_per_index) | b_idx
-            
-        return packed_hash
-    """
 
     def move_to_string(self, move: tuple, mode: StringMode) -> str:
         box_idx, dx, dy = move
@@ -368,67 +287,8 @@ class Sokoban(Game):
         clean_pos = strposition.replace('t', ' ').replace('W', '#').replace("p", "@").replace("b", "$").replace("g", "*").replace("P", "+")
         return clean_pos.replace("\n", "").replace("\r", "")
     
-    #helper functions
     def get_pos_idx(self, position: str):
         p_idx = position.find('@')
         if p_idx == -1:
             p_idx = position.find('+')
         return p_idx; 
-
-
-def benchmark_sweep(variant_id="6", iterations=1000):
-    print(f"--- Benchmarking Sokoban Variant {variant_id} ---")
-
-    # 1. Initialize the game and get the starting position
-    game = Sokoban(variant_id)
-    position = game.start()
-
-    # 2. Warm up (optional, but helps get past initial Python overhead)
-    game.primitive(position)
-
-    # 3. Start the clock
-    start_time = time.perf_counter()
-
-    # 4. Hammer the primitive function
-    for _ in range(iterations):
-        game.primitive(position)
-        
-    # 5. Stop the clock
-    end_time = time.perf_counter()
-
-    # 6. Crunch the numbers
-    total_time = end_time - start_time
-    ms_per_call = (total_time / iterations) * 1000
-    states_per_second = iterations / total_time
-
-    print(f"Total time for {iterations} checks: {total_time:.4f} seconds")
-    print(f"Average time per primitive() call:  {ms_per_call:.4f} ms")
-    print(f"States evaluated per second:        {states_per_second:,.0f} states/sec\n")
-
-# Run the benchmark when you execute the file
-if __name__ == "__main__":
-
-    benchmark_sweep("1", 1000)
-    
-    # Let's do a stress test of 10,000 just for fun
-    benchmark_sweep("1", 10000)
-
-    benchmark_sweep("2", 1000)
-    
-    # Let's do a stress test of 10,000 just for fun
-    benchmark_sweep("2", 10000)
-
-    benchmark_sweep("3", 1000)
-    
-    # Let's do a stress test of 10,000 just for fun
-    benchmark_sweep("3", 10000)
-
-    benchmark_sweep("5", 1000)
-    
-    # Let's do a stress test of 10,000 just for fun
-    benchmark_sweep("5", 10000)
-
-    benchmark_sweep("6", 1000)
-    
-    # Let's do a stress test of 10,000 just for fun
-    benchmark_sweep("6", 10000)
