4147e01a4427f1d679ac50480b7a0bc510230df4
Manlin Zhang <manlinzhang@berkeley.edu>
2026-09-20 13:24:47 -0700

    added primitive win

--- games/src/games/bug.py
+++ games/src/games/bug.py
@@ -1,6 +1,9 @@
 from models import Game, Value, StringMode
 from typing import Optional
 
+"""Position: [player, 0 or 1] * 19 [what player is occupying]
+Move: [move]n - 06, 0710, 0713, 0718, 08, 071114, 071114"""
+
 class Bug(Game):
     id = 'bug'
     variants = ["regular"]
@@ -26,6 +29,10 @@ class Bug(Game):
         """
         Returns a list of positions given the input position.
         """
+        # First, eat all the bugs
+        # Second, return a list of available placement (1, ji)
+
+
         if position == 1:
             return [2, 3]
         elif position == 2:
@@ -38,13 +45,16 @@ class Bug(Game):
         """
         Returns the resulting position of applying move to position.
         """
+        # you get a position like 101201201201201201200000 (20 digits, first digit is player turn indicator)
+        # 130132 <- means change 34 to 1, which is white, change 12 to black
+        #if no moves left, return 10 for white win, or 20 for black win
         return move
 
     def primitive(self, position: int) -> Optional[Value]:
         """
         Returns a Value enum which defines whether the current position is a win, loss, or non-terminal. 
         """
-        if position == 3:
+        if position == 10 or position == 20:
             return Value.Win
         return None
 
@@ -65,6 +75,7 @@ class Bug(Game):
         """
         Returns a string representation of the move based on the given mode.
         """
+        
         return str(move)
 
     
