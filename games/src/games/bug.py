a9aac27a29f1d617e28f7e0ab747fe8f8b8dd192
Manlin Zhang <manlinzhang@berkeley.edu>
2026-09-22 21:00:05 -0700

    added lachlan's generate_move function

--- games/src/games/bug.py
+++ games/src/games/bug.py
@@ -4,6 +4,118 @@ from typing import Optional
 """Position: [if player 1's turn, add 1 to the start; if Player 0, it is implicitly 0] + 19 tiles [what player is occupying] 1 if empty, 2 if white, 3 if black
 Move: [move]n - 06, 0710, 0713, 0718, 08, 071114, 071114"""
 
+class Board:
+    spaces = [(2, 0, 0), (1, 1, 0), (0, 2, 0),
+              (1, 0, -1), (1, 0, 0), (0, 1, 0), (0, 1, 1),
+              (0, 0, -2), (0, 0, -1), (0, 0, 0), (0, 0, 1), (0, 0, 2),
+              (0, -1, -1), (0, -1, 0), (-1, 0, 0), (-1, 0, 1),
+              (0, -2, 0), (-1, -1, 0), (-2, 0, 0)]
+
+    def __init__(self, position, bugs):
+        self.lst = [[[None for _ in range(5)] for _ in range(5)] for _ in range(5)]
+        self.bugs = bugs
+        for index in self.spaces:
+            self.setitem(index, position % 10)
+            position //= 10
+        self.player = 1 if position else 2
+
+    def setitem(self, index, value):
+        self.lst[index[0]+2][index[1]+2][index[2]+2] = value
+
+    def getitem(self, index):
+        return self.lst[index[0]+2][index[1]+2][index[2]+2]
+
+    def untangle(self, index):
+        p = index[0] + index[1]
+        q = index[1] + index[2]
+        if p > 0 and q > 0:
+            j = min(p, q)
+        elif p < 0 and q < 0:
+            j = max(p, q)
+        else:
+            j = 0
+        return (p - j, j, q - j)
+
+    def normalize(self, cells):
+        return min(tuple(sorted(self.untangle((a - x, b - y, c - z)) for a, b, c in cells)) for x, y, z in cells)
+
+    def neighbors(self, index):
+        i = index[0]
+        j = index[1]
+        k = index[2]
+        neighbors = []
+        for di in [-1, 0, 1]:
+            for dj in [-1, 0, 1]:
+                for dk in [-1, 0, 1]:
+                    if abs(di) + abs(dj) + abs(dk) == 1:
+                        n = self.untangle((i + di, j + dj, k + dk))
+                        if n in self.spaces:
+                            neighbors.append(n)
+        return neighbors
+
+    def copy(self):
+        new = Board(0, [])
+        new.lst = [[[k for k in j] for j in i] for i in self.lst]
+        new.player = self.player
+        return new
+
+    def execute_move(self, move, value):
+        new = self.copy()
+        new.setitem(move, value)
+        return new
+
+class Group:
+    def __init__(self, index, board, bugs):
+        self.indexes = [index]
+        self.board = board
+        self.color = board.getitem(index)
+        self.size = 1
+        self.shape = []
+        self.bugs = bugs
+
+    def expand(self):
+        new_indexes = []
+        for index in self.indexes:
+            for neighbor in self.board.neighbors(index):
+                if self.board.getitem(neighbor) == self.color and neighbor not in self.indexes and neighbor not in new_indexes:
+                    new_indexes.append(neighbor)
+        self.indexes.extend(new_indexes)
+        self.size = len(self.indexes)
+        if new_indexes:
+            self.expand()
+        self.shape = self.board.normalize(self.indexes)
+
+    def disassemble(self, board):
+        for index in self.indexes:
+            board.setitem(index, 0)
+
+    def same_shape(self, bug):
+        target = bug.shape
+        shape = list(self.indexes)
+        for _ in range(6):
+            if self.board.normalize(shape) == target or self.board.normalize([(k, j, i) for i, j, k in shape]) == target:
+                return True
+            shape = [self.board.canon((-k, i, j)) for i, j, k in shape]
+        return False
+
+    def try_eat(self):
+        gone = []
+        for index in self.indexes:
+            for neighbor in self.board.neighbors(index):
+                if self.board.getitem(neighbor) != self.color and self.board.getitem(neighbor) != 0:
+                    for bug in self.bugs:
+                        if neighbor in bug.indexes and bug.color != self.color and bug.size == self.size and bug not in gone and self.same_shape(bug):
+                            gone.append(bug)
+        return gone
+
+    def try_grow(self, board):
+        moves = []
+        for index in self.indexes:
+            for point in board.neighbors(index):
+                if board.getitem(point) == 0 and point not in moves and all(board.getitem(n) != self.color or n in self.indexes for n in board.neighbors(point)):
+                    moves.append(point)
+        return moves
+
 class Bug(Game):
     id = 'bug'
     variants = ["regular"]
@@ -24,24 +136,103 @@ class Bug(Game):
         Returns the starting position of the game.
         """
         return 1
-    
+
     def generate_moves(self, position: int) -> list[int]:
         """
         Returns a list of positions given the input position.
         """
         # First, eat all the bugs
         # Second, return a list of available placement (1, ji)
-        origin = [0,0,0]
-        board = Board(position)
+        bugs = []
+        board = Board(position, bugs)
         player = board.player
-        if position == 1:
-            return [2, 3]
-        elif position == 2:
-            return [4, 5]
-        elif position == 4:
-            return [2]
-        return []
-    
+        postmoves = []
+        maxsize = 0
+        PostMultiverse = []
+        CombinationalMoves = []
+        for spot in board.spaces:
+            if board.getitem(spot) != 0 and not any(spot in bug.indexes for bug in bugs):
+                new = Group(spot, board, bugs)
+                new.expand()
+                bugs.append(new)
+        for bug in bugs:
+            if bug.size > maxsize:
+                maxsize = bug.size
+
+        moves = [spot for spot in board.spaces if board.getitem(spot) == 0]
+        for bug in bugs:
+            if bug.color == player:
+                for occupied in bug.indexes:
+                    for neighbor in board.neighbors(occupied):
+                        if bug.size == maxsize and neighbor in moves:
+                            moves.remove(neighbor)
+        for move in list(moves):
+            if len([bug for bug in bugs if bug.color == player and any(neighbor in bug.indexes for neighbor in board.neighbors(move))]) > 1:
+                moves.remove(move)
+        for move in moves:
+            possibleboard = board.execute_move(move, player)
+            PostMultiverse.append(possibleboard)
+            postmoves.append([move])
+
+        while PostMultiverse:
+            optionboard = PostMultiverse.pop()
+            premoves = postmoves.pop()
+            bugs = []
+            for spot in optionboard.spaces:
+                if optionboard.getitem(spot) != 0 and not any(spot in bug.indexes for bug in bugs):
+                    new = Group(spot, optionboard, bugs)
+                    new.expand()
+                    bugs.append(new)
+            eaters = [bug for bug in bugs if bug.color == player and bug.try_eat()]
+            changed = True
+            while changed:
+                changed = False
+                eatboard = optionboard.copy()
+                gone = []
+                for bug in eaters:
+                    for victim in bug.try_eat():
+                        if victim not in gone:
+                            gone.append(victim)
+                for victim in gone:
+                    victim.disassemble(eatboard)
+                for bug in eaters:
+                    if not bug.try_grow(eatboard):
+                        eaters.remove(bug)
+                        changed = True
+                        break
+            combinationalmoves = [[]]
+            for bug in eaters:
+                extended = []
+                for prefix in combinationalmoves:
+                    growboard = eatboard.copy()
+                    for spot in prefix:
+                        growboard.setitem(spot, player)
+                    for spot in bug.try_grow(growboard):
+                        extended.append(prefix + [spot])
+                combinationalmoves = extended
+            if not eaters or not combinationalmoves:
+                CombinationalMoves.append(premoves)
+            else:
+                for suffix in combinationalmoves:
+                    possibleboard = eatboard.copy()
+                    for spot in suffix:
+                        possibleboard.setitem(spot, player)
+                    PostMultiverse.append(possibleboard)
+                    postmoves.append(premoves + suffix)
+
+        combinationalstrings = []
+        for sequence in CombinationalMoves:
+            digits = str(player)
+            for move in sequence:
+                tile = board.spaces.index(move) + 1
+                if tile < 10:
+                    digits += "0" + str(tile)
+                else:
+                    digits += str(tile)
+            combinationalstrings.append(int(digits))
+        return combinationalstrings
+
+
     def do_move(self, position: int, move: int) -> int:
         """
         Returns the resulting position of applying move to position.
@@ -74,12 +265,12 @@ class Bug(Game):
 
         #swap player turn
         updated_pos_string = str(abs(player_turn - 1)) + pos_str_clean
-        
+
         return int(updated_pos_string)
 
     def primitive(self, position: int) -> Optional[Value]:
         """
-        Returns a Value enum which defines whether the current position is a win, loss, or non-terminal. 
+        Returns a Value enum which defines whether the current position is a win, loss, or non-terminal.
         """
         if position == 10 or position == 20:
             return Value.Win
@@ -102,122 +293,5 @@ class Bug(Game):
         """
         Returns a string representation of the move based on the given mode.
         """
-        
+
         return str(move)
-
-
-
-    class Board:
-            def __init__(self, position):
-                self.lst = [[[None for _ in range(5)] for _ in range(5)] for _ in range(5)]
-                for i in range(5):
-                    for j in range(5):
-                        for k in range(5):
-                            if abs((i -2) + (j - 2) + (k - 2)) > 2:
-                                pass
-                            self.lst[i][j][k] = position % 10
-                            position //= 10
-                self.player = position
-                
-                
-            def setitem (self, index, value):
-                self.lst[index[0]-2][index[1]-2][index[2]-2] = value
-
-            def getitem(self, index):
-                return self.lst[index[0]-2][index[1]-2][index[2]-2]
-
-            def neighbors(self, index):
-                i = index[0]
-                j = index[1]
-                k = index[2]
-                neighbors = []
-                for di in [-1, 0, 1]:
-                    for dj in [-1, 0, 1]:
-                        for dk in [-1, 0, 1]:
-                            if abs(di) + abs(dj) + abs(dk) == 1:
-                                ni, nj, nk = i + di, j + dj, k + dk
-                                if 0 <= ni < 5 and 0 <= nj < 5 and 0 <= nk < 5:
-                                    neighbors.append((ni, nj, nk))
-                return neighbors
-
-    class Group:
-        def __init__(self, index, board, bugs):
-            self.indexes = [index]
-            self.board = board
-            self.color = board.getitem(index)
-            self.size = 1
-            self.shape = ['', '', '']
-            self.bugs = bugs
-
-        def expand(self):
-            new_indexes = []
-            for index in self.indexes:
-                for neighbor in self.board.neighbors(index):
-                    if self.board.getitem(neighbor) == self.color and neighbor not in self.indexes and neighbor not in new_indexes:
-                        new_indexes.append(neighbor)
-                        if neighbor[0] < self.shape[0]:
-                            self.shape[0].append('-i')
-                        if neighbor[0] > self.shape[0]:
-                            self.shape[0].append('+i')
-                        if neighbor[1] < self.shape[1]:
-                            self.shape[1].append('-j')
-                        if neighbor[1] > self.shape[1]:
-                            self.shape[1].append('+j')
-                        if neighbor[2] < self.shape[2]:
-                            self.shape[2].append('-k')
-                        if neighbor[2] > self.shape[2]:
-                            self.shape[2].append('+k')
-            self.indexes.extend(new_indexes)
-            self.size = len(self.indexes)
-            if new_indexes:
-                self.expand()
-
-            def disassemble(self):
-                for index in self.indexes:
-                    self.board.setitem(index, 0)
-
-            def same_shape(self, bug):
-                axes = "ijk"
-                def parse(term):
-                    signs, sign = [], 1
-                    for ch in "".join(str(term).split()):
-                        if ch == "+":
-                            sign = 1
-                        elif ch == "-":
-                            sign = -1
-                        elif ch in axes:
-                            signs.append(sign)
-                        return tuple(signs)
-
-                def flip(slot):
-                    return tuple(-s for s in slot)
-
-                target = [parse(t) for t in bug.shape]
-                shape = [parse(t) for t in self.shape]
-
-                for _ in range(6):
-                    if shape == target:
-                        return True
-                    shape = [flip(shape[2]), shape[0], shape[1]]
-
-                return False
-
-            def try_eat(self):
-                gone = []
-                moves = [[],[],[],[],[],[]]
-                for index in self.indexes:
-                    for neighbor in self.board.neighbors(index):
-                        if self.board.getitem(neighbor) != self.color and self.board.getitem(neighbor) != 0:
-                            for bug in self.bugs:
-                                if neighbor in bug.indexes and bug.color != self.color and bug.size < self.size and self.same_shape(bug):
-                                    gone.append(bug)
-                i = 0
-                for bug in gone:
-                    for bug_index in bug.indexes:
-                        for point in self.board.neighbors(bug_index):
-                            if self.board.getitem(point) == 0 or point in bug.indexes:
-                                moves[bug_index[i]].append(point)
-                    i += 1
-                return moves
-
-    
